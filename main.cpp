b82be8a94e72e8f6dd40c58efcfa55debb93b515
shift-runstop <shift-runstop@0xFF>
2021-09-19 00:13:07 +0100

    execute, byte-fetching , read-byte from memory functions added. Opcode section started with LDA, bit of refactoring too

--- main.cpp
+++ main.cpp
@@ -3,18 +3,36 @@
 
 // timestamp 17:09
 
-using Byte = unsigned char;
-using Word = unsigned short;
-using u32 = unsigned int;
+using Byte = unsigned char; // 8 bits
+using Word = unsigned short; // 16 bits
+using u32 = unsigned int; // workaround for memory intialise function
+
+struct MEM{
+    static constexpr u32 MAX_MEM = 1024 * 64;
+    Byte data[MAX_MEM];
+
+    void initialise() {
+        for ( u32 i = 0; i < MAX_MEM; i++){
+            data[i] = 0;
+        }
+    }
+
+	// get a byte out of memory (hacky, needs fix)
+	Byte operator[] ( u32 Address ) const {
+		return data[Address];
+	}
+};
 
 struct CPU{
-    
+
+	//http://www.obelisk.me.uk/6502/registers.html
     Word PC; // Program counter
     Word SP; // Stack Pointer
 
     Byte A, X, Y; // Registers
 
-    // Status Flags set by bits
+    // Processor status flags set by bits
+	// set up as Bitfields
     Byte C : 1; // Carry
     Byte Z : 1; // Zero
     Byte I : 1; // Interrupt Disable
@@ -23,30 +41,47 @@ struct CPU{
     Byte V : 1; // Overflow
     Byte N : 1; // Negative
 
-    void Reset( MEM& memory){
+    void reset( MEM& memory){
+		// c64-wiki.com/wiki/Reset_(Process)
         // Reset Vector Address $FFFC
         PC = 0xFFFC;
-        SP = 0x0100;
+        SP = 0x0100; // starts off in zero page at $00FF but moves to top of stack at $0100
         C = I = Z = D = B = V = N = 0;
         A = X = Y =0;
-        memory.Initialise();        
+        memory.initialise();
     }
+
+	Byte getByte(u32& clockCycles, MEM& memory){
+		Byte data = memory[PC]; // read program counter
+		PC++;
+		clockCycles--; // clock tends towards zero so instruction can finish for now
+		return data;
+	}
+
+	static constexpr Byte
+		OP_LDA_IM = 0xA9; // Load Accumulator in immediate mode setting zero and negative flags as appropriate
+
+	void execute( u32 clockCycles, MEM& memory){
+		while(clockCycles > 0){
+
+			Byte instruction = getByte(clockCycles, memory);
+			switch(instruction){
+				case OP_LDA_IM:{
+					Byte Value = getByte(clockCycles, memory);
+					A = Value;
+					Z = (A==0); // if A is 0
+					N = (A & 0b1000000) > 0; // if bit 7 of A is set
+				} break;
+			}
+		}
+	}
 };
 
-struct MEM{
-    static constexpr u32 MAX_MEM = 1024 * 64;
-    Byte Data[MAX_MEM];
-    
-    void Initialise() {
-        for ( u32 i = 0; i < MAX_MEM; i++){
-            Data[i] = 0;
-        }
-    }
-};
 
 int main() {
     MEM mem;
     CPU cpu;
-    cpu.Reset( mem );
+    cpu.reset( mem );
+	cpu.execute( 2, mem ); // 2 cycles needed for accumulator http://www.obelisk.me.uk/6502/reference.html#LDA
     return 0;
 }
